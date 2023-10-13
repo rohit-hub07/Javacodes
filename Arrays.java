2bc2378534b626d56e8a83318e19f9e125967a09
rohit-hub07 <rohitsingh986491@gmail.com>
2023-10-13 11:10:04 +0530

    add arrays.java

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+ class Arrays {
+    
+    public static void main(String args[]){
+        int[] marks= new int[3];
+        
+        marks[0] = 67;
+        marks[1] = 70;
+        marks[2] = 78;
+
+        //System.out.println(marks[0]);
+        //System.out.println(marks[1]);
+        //System.out.println(marks[2]);
+
+        //or 
+        for(int i =0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+    }
+
